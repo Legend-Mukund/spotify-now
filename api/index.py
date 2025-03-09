73825dcae883c73700492161d22395f65b23b009
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <themukundsinghrajput@gmail.com>
2025-03-09 15:04:37 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -96,4 +96,5 @@ def spotify():
         resp.headers["Cache-Control"] = "public, max-age=0, must-revalidate"
         return resp
 
-app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
